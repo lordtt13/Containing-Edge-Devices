5e0996ddb4b4e7f0c68437529c8e26cfa3712c46
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-11-05 13:40:09 +0530

    Added real world data

--- parametrizing.py
+++ parametrizing.py
@@ -4,6 +4,10 @@ Created on Sat Oct 12 23:55:51 2019
 
 @author: tanma
 """
+import pandas as pd
+
+
+data = pd.read_csv("data.csv")
 
 def threshold(arr,mean_arr,n):
     j = 0.01
@@ -13,3 +17,5 @@ def threshold(arr,mean_arr,n):
         j += 0.01
     return min(excess),max(excess)
 
+min_val , max_val = threshold(data["23.0"], data["23.0"].mean(), 0.75)
+min_val , max_val = threshold(data["64.0"], data["64.0"].mean(), 0.75)
