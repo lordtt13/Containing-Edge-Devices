da4e308e2ddb6b931bbd764b8674ff35bed3b4ea
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-18 22:50:24 +0530

    Data structure, function_call add

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 19:35:59 2020
+
+@author: Tanmay Thakur
+"""
+import numpy as np
+
+from parametrizing import threshold
+
+
+timestep = 60
+
+def append_new(data, point):
+    data.append(point)
+    return data.pop(0)
+
+    
+data_temp, data_humid = [0 for _ in range(timestep)], [0 for _ in range(timestep)]
+
+for _ in range(timestep):
+    # dict_new = function_call()
+    for key, value in dict_new.items():
+        if(key == "Temperature"):
+            append_new(data_temp, value)
+            print(threshold(data_temp, np.mean(data_temp), 0.75))
+        else:
+            append_new(data_humid, value)
+            print(threshold(data_humid, np.mean(data_humid), 0.75))
